556acfcd50b0d11fb7a4007ec17f34ca73ee8c97
Brayan López <brxyxn.corp@live.com>
2024-05-02 06:33:00 -0600

    feat(server): update server cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	server "github.com/brxyxn/go-phonebook-api/internal/app"
+	"github.com/brxyxn/go-phonebook-api/internal/constants"
+	"github.com/brxyxn/go-phonebook-api/pkg/logger"
+	"os"
+)
 
 func main() {
-	// Start the server
-	fmt.Println("Starting server...")
+	a := server.NewServer()
+
+	// Logger configuration
+	cfg := logger.LogConfig{
+		Out: os.Stdout,
+		LogOptions: logger.LogOptions{
+			IsDebug:  true,
+			LogLevel: "debug",
+			CallerOptions: logger.CallerOptions{
+				WithCaller:       true,
+				WithCustomCaller: true,
+				BasePath:         "go-phonebook-api",
+			},
+			DateTimeOptions: logger.DateTimeOptions{
+				WithTime:   true,
+				TimeFormat: constants.LongDateTimeFormat,
+			},
+		},
+	}
+	simpleLogger := logger.NewLogger(cfg)
+	a.Logger = &simpleLogger
+
+	a.Configure()
+
+	a.Start()
 }
